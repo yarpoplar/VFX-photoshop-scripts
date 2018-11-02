04d5aff42d2abb06100c29f90a67d1efff1f35db
Oleg Ermakov <oleg.ermakov@akvelon.com>
2018-11-02 20:38:55 +0300

    Code style fixes

--- SaveToPNG.jsx
+++ SaveToPNG.jsx
@@ -8,53 +8,55 @@
 
 main();
 
-function main()
-{
+function main() {
 	if(!documents.length) 
 		return;
 
 	var isPSDSaved = true;
 
-	try
-	{
-	    var Path = decodeURI(activeDocument.path);
+	try	{
+		var Path = decodeURI(activeDocument.path);
 	}
-	catch(e)
-	{
+	catch(e) {
 		var Path = Folder.selectDialog("Select folder to save");
+
 		isPSDSaved = false;
 		//alert(e); 
 		//return;
 	}
 
-	if(!Folder(Path).exists)
-	{
-	    alert(Path + " Does not exist!");
+	if(!Folder(Path).exists) {
+		alert(Path + " Does not exist!");
+		
 	    return;
 	}
 
 	var Name = decodeURI(app.activeDocument.name).replace(/\.[^\.]+$/, '');
 	var SaveName = prompt("Type in the name of the file", Name); 
 	var saveFile = File(Path + "/" + SaveName + ".png");
+
 	SaveToPNG24(saveFile);
 
 	var psdSaveName = isPSDSaved ? Name : SaveName;
+
 	saveFile = File(Path + "/" + psdSaveName + ".psd");
 	SaveToPSD(saveFile);
 }
 
-function SaveToPNG24(saveFile)
-{
+function SaveToPNG24(saveFile) {
 	var pngSaveOptions = new PNGSaveOptions;
+
 	pngSaveOptions.compression = 9;
 	pngSaveOptions.interlaced = false;
+
 	activeDocument.saveAs(saveFile, pngSaveOptions, true, Extension.LOWERCASE);
 }
 
-function SaveToPSD(saveFile)
-{
+function SaveToPSD(saveFile) {
 	psdSaveOptions = new PhotoshopSaveOptions();
+
 	psdSaveOptions.embedColorProfile = true;
 	psdSaveOptions.alphaChannels = true;
+
 	activeDocument.saveAs(saveFile, psdSaveOptions, false, Extension.LOWERCASE);
 }
--- SaveToTGA.jsx
+++ SaveToTGA.jsx
@@ -8,58 +8,60 @@
 
 main();
 
-function main()
-{
+function main() {
 	if(!documents.length) 
 		return;
 
 	if (app.activeDocument.bitsPerChannel==BitsPerChannelType.SIXTEEN) {
 		alert("TGA does not support 16 bit color!"); 
+		
 		return;
 	}
 
 	var isPSDSaved = true;
 
-	try
-	{
-	    var Path = decodeURI(activeDocument.path);
+	try {
+		var Path = decodeURI(activeDocument.path);
 	}
-	catch(e)
-	{
+	catch(e) {
 		var Path = Folder.selectDialog("Select folder to save");
+
 		isPSDSaved = false;
 		//alert(e); 
 		//return;
 	}
 
-	if(!Folder(Path).exists)
-	{
-	    alert(Path + " Does not exist!");
-	    return;
+	if(!Folder(Path).exists) {
+		alert(Path + " Does not exist!");
+		
+		return;
 	}
 
 	var Name = decodeURI(app.activeDocument.name).replace(/\.[^\.]+$/, '');
 	var SaveName = prompt("Type in the name of the file", Name); 
 	var saveFile = File(Path + "/" + SaveName + ".tga");
+
 	SaveToTGA(saveFile);
 	
 	var psdSaveName = isPSDSaved ? Name : SaveName;
+
 	saveFile = File(Path + "/" + psdSaveName + ".psd");
 	SaveToPSD(saveFile);
 }
 
-function SaveToTGA(saveFile)
-{
+function SaveToTGA(saveFile) {
 	targaSaveOptions = new TargaSaveOptions();  
 	targaSaveOptions.alphaChannels = true;  
-	targaSaveOptions.resolution = TargaBitsPerPixels.THIRTYTWO;  
+	targaSaveOptions.resolution = TargaBitsPerPixels.THIRTYTWO;
+
 	activeDocument.saveAs(saveFile, targaSaveOptions, true, Extension.LOWERCASE);
 }
 
-function SaveToPSD(saveFile)
-{
+function SaveToPSD(saveFile) {
 	psdSaveOptions = new PhotoshopSaveOptions();
+
 	psdSaveOptions.embedColorProfile = true;
 	psdSaveOptions.alphaChannels = true;
+
 	activeDocument.saveAs(saveFile, psdSaveOptions, false, Extension.LOWERCASE);
 }
